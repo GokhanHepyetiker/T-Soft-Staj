e23cb43599912e995e62e39a55405cbe5a329fe0
GokhanHepyetiker <gokhanhepyetiker@gmail.com>
2024-08-14 15:50:24 +0300

    ProgressSteps

---
+++ Progressteps/script.js
@@ -0,0 +1,31 @@
+const circles = document.querySelectorAll('.circle')
+progressBar = document.querySelector('.indicator')
+buttons = document.querySelectorAll('button')
+
+let currentStep = 1;
+
+const updateSteps = (e) => {
+
+    currentStep = e.target.id === "Next" ? currentStep+1 : currentStep-1;
+ 
+ 
+ circles.forEach((circle, index) => {
+    console.log(currentStep);
+    circle.classList[`${index < currentStep ? 'add' : 'remove'}`]('active');
+  });  
+
+  progressBar.style.width = `${(currentStep - 1) / (circles.length - 1) * 100}%`;
+
+  if(currentStep === circles.length){
+    buttons[1].disabled = true;
+  } else if(currentStep === 1){
+    buttons[0].disabled = true;
+  } else {
+    buttons.forEach((button) => (button.disabled = false));
+  }
+    
+};
+
+buttons.forEach((button) => {
+    button.addEventListener("click", updateSteps);
+});
