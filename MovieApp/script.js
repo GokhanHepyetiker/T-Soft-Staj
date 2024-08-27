6fe75a0da335a47e54edf3b8ce8e99dc87859441
GokhanHepyetiker <gokhanhepyetiker@gmail.com>
2024-08-27 13:47:32 +0300

    MovieApp
    MovieApp with js and flexible responsive.

--- MovieApp/script.js
+++ MovieApp/script.js
@@ -34,7 +34,7 @@ function showMovies(movies) {
                 <img src="${IMG_PATH + poster_path}" alt="${title}">
                 <div class="movie-info">
             <h3>${title}</h3>
-            <span class="${getClassByRate(roundedVote)}">${roundedVote}</span> <!-- Yuvarlanmış puanı kullan -->
+            <span class="${getClassByRate(roundedVote)}">${roundedVote}</span> 
                 </div>
                 <div class="overview">
             <h3>Overview</h3>
